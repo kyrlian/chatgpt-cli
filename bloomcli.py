b9e52a431c930a08779f73365cc80ca776c1ae3e
kyrlian <kyrlian@gmail.com>
2023-03-04 19:16:18 +0100

    print

--- bloomcli.py
+++ bloomcli.py
@@ -11,13 +11,14 @@ headers = {"Authorization": "Bearer "+api_key}
 def get_response(input):
     payload =  {"inputs": input}
     response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()[0]['generated_text'].strip()
-	
+    generated_text = response.json()[0]['generated_text'].strip()
+    print(generated_text)
+    return generated_text
+
 def main(firstinput):
     res=""
     if firstinput != "":
         res = get_response(firstinput)
-        print(res)
     while True:
         user_input = input("> ").strip()
         if user_input in ["quit", "exit", "bye"]:
@@ -26,8 +27,6 @@ def main(firstinput):
             res=""
             user_input = input("> ").strip()
         res = get_response(res + " " + user_input)
-        print(res)
-        #print(response)
 
 if __name__ == "__main__":
     args = sys.argv[1:]
